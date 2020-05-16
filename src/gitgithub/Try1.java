13b2a44ab61c4af7e4dbda80884b4b4fc18143cf
Davi <davi-rocha2@hotmail.com>
2020-05-16 19:16:39 -0300

    add mensagem Hello

---
+++ src/gitgithub/Try1.java
@@ -0,0 +1,15 @@
+package gitgithub;
+import java.util.Scanner;
+
+public class Try1 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		System.out.println("Hello");
+		
+		sc.close();	
+
+	}
+
+}
